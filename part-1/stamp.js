63c19c4f7aaad2caac6ea01608d31cf2e4f96a55
Alex-E-Camacho <alex.e.camacho@gmail.com>
2017-05-21 16:39:02 -0400

    Function averageAppriasal passes jasmine test

--- part-1/stamp.js
+++ part-1/stamp.js
@@ -2,4 +2,9 @@ var Stamp = function(args = {}) {
   this.name = args.name;
   this.issueDate = args.issueDate;
   this.appraisalValues = args.appraisalValues || [];
-}
+};
+
+Stamp.prototype.averageAppraisal = function() {
+  var total = this.appraisalValues.reduce(function(memo, val) {return memo + val}, 0);
+  return total/this.appraisalValues.length;
+};
