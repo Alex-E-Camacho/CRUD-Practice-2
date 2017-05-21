00c2ee38c7e510798ce8335b5e383e1aa122521a
Alex-E-Camacho <alex.e.camacho@gmail.com>
2017-05-21 17:28:26 -0400

    .value function passes, all jasmine tests pass

--- part-1/stamp_collection.js
+++ part-1/stamp_collection.js
@@ -8,3 +8,13 @@ StampCollection.prototype.stampNamed = function(searchedName) {
   }
   return this.stamps.find(nameMatches);
 };
+
+StampCollection.prototype.stampsIssued = function(searchedDate) {
+  return this.stamps.filter(function(stamp) {
+    return stamp.issueDate === searchedDate;
+  })
+};
+
+StampCollection.prototype.value = function() {
+  return this.stamps.reduce(function(memo, stamp) {return memo + stamp.maximumAppraisal()},0);
+}
